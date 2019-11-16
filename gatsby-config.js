4d2aa5f6243ec2b68ecb02696f755c533206ffb0
Callum Macpherson <callummac@protonmail.com>
2019-11-16 09:03:03 +0000

    Added siteMetedata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,7 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: `thecallum.com`,
+    siteUrl: `https://thecallum.com`,
+    description: `Portfolio site build on Gatsby`,
+  },
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,22 @@
 import React from "react"
+import { graphql } from "gatsby"
 
-export default () => <div>Hello world!</div>
+export default ({ data }) => {
+  const { title } = data.site.siteMetadata
+
+  return (
+    <div>
+      <h1>{title}</h1>
+    </div>
+  )
+}
+
+export const query = graphql`
+  {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`

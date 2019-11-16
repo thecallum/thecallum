d8a612aaec2ad4ff7a1ac6f66671bb1adbfccb97
Callum Macpherson <callummac@protonmail.com>
2019-11-16 09:06:34 +0000

    Initiated page layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,15 @@
 import React from "react"
 import { graphql } from "gatsby"
 
+import Layout from "../components/layout"
+
 export default ({ data }) => {
   const { title } = data.site.siteMetadata
 
   return (
-    <div>
+    <Layout>
       <h1>{title}</h1>
-    </div>
+    </Layout>
   )
 }
 

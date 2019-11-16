c5aaefb9b070b0ec886fe0ced91739c2cf574e7d
Callum Macpherson <callummac@protonmail.com>
2019-11-16 10:50:10 +0000

    Connected Contentful, added projects to /projects page

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,4 +4,15 @@ module.exports = {
     siteUrl: `https://thecallum.com`,
     description: `Portfolio site build on Gatsby`,
   },
+
+  plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `j6go2m3dzf1w`,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: `w2iwh3PA0Qv1SFm8ogT_BK27mznLynx9rHMzTQg-zvc`,
+      },
+    },
+  ],
 }
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,10 +1,41 @@
 import React from "react"
+import { graphql, Link } from "gatsby"
 import Layout from "../components/layout"
 
-export default () => {
+export default ({ data }) => {
+  const { totalCount, nodes: projects } = data.allContentfulProjects
+
   return (
     <Layout>
       <h1>Projects</h1>
+      <p>Number of projects: {totalCount}</p>
+
+      <ul>
+        {projects.map(({ title, slug }) => (
+          <li>
+            <Link to={`/projects/${slug}`}>{title}</Link>
+          </li>
+        ))}
+      </ul>
     </Layout>
   )
 }
+
+export const projects = graphql`
+  {
+    allContentfulProjects(filter: { visible: { eq: true } }) {
+      nodes {
+        slug
+        title
+        summary
+        published(formatString: "Do MMMM YYYY")
+        thumbnail {
+          fluid {
+            src
+          }
+        }
+      }
+      totalCount
+    }
+  }
+`

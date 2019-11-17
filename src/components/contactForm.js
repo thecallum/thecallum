fe6154e5cb0487a110698aca95a18efabf2e18a6
Callum Macpherson <callummac@protonmail.com>
2019-11-17 12:46:23 +0000

    Added contact form

---
+++ src/components/contactForm.js
@@ -0,0 +1,30 @@
+import React from "react"
+
+export default () => {
+  return (
+    <form data-netlify="true">
+      <div>
+        <label>
+          Name
+          <input type="text" name="name" />
+        </label>
+      </div>
+
+      <div>
+        <label>
+          Email
+          <input type="text" name="email" />
+        </label>
+      </div>
+
+      <div>
+        <label>
+          Message
+          <textarea name="message"></textarea>
+        </label>
+      </div>
+
+      <button type="submit">Send</button>
+    </form>
+  )
+}
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,21 @@
 import React from "react"
 import Layout from "../components/layout"
+import Form from "../components/contactForm"
 
 export default () => {
   return (
     <Layout>
-      <h1>Contact</h1>
+      <h1>Contact Me</h1>
+
+      <p>Have a question?</p>
+      <p>Or send me an email at hello@thecallum.com</p>
+
+      <Form />
+
+      <ul>
+        <li>LinkedIn</li>
+        <li>Github</li>
+      </ul>
     </Layout>
   )
 }

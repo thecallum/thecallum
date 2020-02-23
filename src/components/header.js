7cc702d38254f5d96c4c270f64fc284b544495b0
Callum <callummac@protonmail.com>
2020-02-23 13:45:02 +0000

    Home page responsive

--- src/components/header.js
+++ src/components/header.js
@@ -2,18 +2,27 @@ import React from "react"
 import links from "../constants/links"
 import { Link } from "gatsby"
 
-export default ({ children }) => (
-  <header className="header">
-    <Link to="/">
-      <img className="header-logo" src="/logo.png" alt="" />
-    </Link>
+export default ({ location }) => {
+  const isCurrentPage = link => {
+    return window.location.pathname == link.path
+  }
 
-    <ul class="header-links">
-      {links.map(link => (
-        <li class="header-link">
-          <Link to={link.path}>{link.name}</Link>
-        </li>
-      ))}
-    </ul>
-  </header>
-)
+  return (
+    <header className="header">
+      <div className="header-main">
+        <Link to="/">
+          <img className="header-icon" src="/logo.svg" alt="" />
+        </Link>
+        <span className="header-logo">thecallum.com</span>
+      </div>
+
+      <ul class="header-links">
+        {links.map(link => (
+          <li className={`header-link ${isCurrentPage(link) ? "active" : ""}`}>
+            <Link to={link.path}>{link.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </header>
+  )
+}

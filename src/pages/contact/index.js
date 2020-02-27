e2b2ad6d36b7adc5f2f4199878050e9f25bcc533
Callum <callummac@protonmail.com>
2020-02-27 21:27:12 +0000

    Added socialk links

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,152 +1,34 @@
 import React, { useState } from "react"
 import { Link, navigate } from "gatsby"
-import Validator from "validatorjs"
 import Layout from "../../components/layout"
 
-import TextAreaField from "../../components/textAreaField"
-import TextInputField from "../../components/textInputField"
+import ContactForm from "../../components/contactForm"
 
-import LoadingIcon from "../../components/loadingIcon"
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 export default () => {
-  const [requestError, setRequestError] = useState(null)
-  const [loading, setLoading] = useState(false)
-
-  const [values, setValues] = useState({
-    name: "",
-    email: "",
-    message: "",
-  })
-
-  const [errors, setErrors] = useState({})
-
-  const handleSubmit = e => {
-    e.preventDefault()
-
-    if (loading || !validateForm()) return
-
-    setRequestError(null)
-    setLoading(true)
-
-    sendRequest({ ...values })
-      .then(response => {
-        setTimeout(() => {
-          setLoading(false)
-          navigate("/contact/success/")
-        }, 1000)
-      })
-      .catch(() => {
-        console.log("error sending request")
-        setRequestError("Unknown error! Try again.")
-        setLoading(false)
-      })
-  }
-
-  const encode = data => {
-    return Object.keys(data)
-      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-      .join("&")
-  }
-
-  const sendRequest = messageContent => {
-    const FETCH_TIMEOUT = 10000
-    let didTimeout = false
-
-    return new Promise((resolve, reject) => {
-      const timeout = setTimeout(() => {
-        didTimeout = true
-        reject(new Error("Request timed out"))
-      }, FETCH_TIMEOUT)
-
-      fetch("/", {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: encode({
-          "form-name": "contact",
-          ...messageContent,
-        }),
-      })
-        .then(response => {
-          clearTimeout(timeout)
-          if (didTimeout) return
-          if (response.status === 404) reject(response)
-          resolve(response)
-        })
-        .catch(error => {
-          if (didTimeout) return
-          console.log({ error })
-
-          reject(error)
-        })
-    })
-  }
-
-  const updateField = e => {
-    setValues({ ...values, [e.target.name]: e.target.value })
-  }
-
-  const validateForm = () => {
-    let data = {
-      name: values.name,
-      email: values.email,
-      message: values.message,
-    }
-
-    let rules = {
-      name: "required",
-      email: "required|email",
-      message: "required|max:2000",
-    }
-
-    let validation = new Validator(data, rules)
-
-    const valid = validation.passes()
-    setErrors({ ...validation.errors.errors })
-
-    return valid
-  }
-
   return (
     <Layout>
       <div className="container">
         <h1>Contact</h1>
 
-        <form onSubmit={handleSubmit} className="form">
-          <TextInputField
-            name="name"
-            field={values.name}
-            error={errors.name}
-            update={updateField}
-            loading={loading}
-          />
-          <TextInputField
-            name="email"
-            field={values.email}
-            error={errors.email}
-            update={updateField}
-            disabled={loading}
-          />
-          <TextAreaField
-            name="message"
-            field={values.message}
-            error={errors.message}
-            update={updateField}
-            disabled={loading}
-          />
-          {requestError && <p className="form-error">{requestError}</p>}
+        <div className="contact-links">
+          <a
+            href="https://www.linkedin.com/in/callum-macpherson-aa0209159/"
+            className="contact-link"
+          >
+            <FontAwesomeIcon icon={faLinkedin} className="contact-link-icon" />
+            <div className="contact-link-label">Linkedin</div>
+          </a>
 
-          <div className="contact-control">
-            <button
-              className="button contact-control-button"
-              type="submit"
-              disabled={loading}
-            >
-              Submit
-            </button>
+          <a href="https://github.com/thecallum" className="contact-link">
+            <FontAwesomeIcon icon={faGithub} className="contact-link-icon" />
+            <div className="contact-link-label">Github</div>
+          </a>
+        </div>
 
-            {loading && <LoadingIcon />}
-          </div>
-        </form>
+        <ContactForm />
       </div>
     </Layout>
   )

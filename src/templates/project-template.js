b486aacff46b4cb6dc8665c02445831e89269867
Callum <callummac@protonmail.com>
2020-02-29 13:17:56 +0000

    Added project template

--- src/templates/project-template.js
+++ src/templates/project-template.js
@@ -1,10 +1,11 @@
 import React from "react"
-// import Layout from "../components/layout"
-// import Technologies from "../components/projectTechnologies"
+import Layout from "../components/layout"
 import { MDXRenderer } from "gatsby-plugin-mdx"
-import { Link, graphql } from "gatsby"
-// import Image from "gatsby-image"
-// import SEO from "../components/seo"
+import { graphql } from "gatsby"
+
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
+import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 export const query = graphql`
   query($id: String!) {
@@ -39,42 +40,53 @@ export default ({ data }) => {
     published,
     summary,
     technologies,
-    image,
     github,
     website,
   } = data.project
 
   const mdx = body.childMdx.body
 
-  return <div>Project template</div>
+  return (
+    <Layout>
+      <div className="container">
+        <h1 className="project-summary-title">{title}</h1>
 
-  // return (
-  //   <Layout>
-  //     <SEO title={title} description={summary} />
-  //     <div
-  //       style={{
-  //         margin: "30px 0",
-  //         backgroundColor: "#f9f9f9",
-  //         padding: "30px",
-  //       }}
-  //     >
-  //       <MDXRenderer>{mdx}</MDXRenderer>
-  //     </div>
+        <div className="project-summary-published">{published}</div>
 
-  //     <p>
-  //       <a href={github}>Github</a>
-  //     </p>
-  //     <p>
-  //       <a href={website}>Website</a>
-  //     </p>
-  //     <p>Published: {published}</p>
-  //     <p>Sumary: {summary}</p>
-  //     <p>Technologies</p>
-  //     <Technologies technologies={technologies} />
+        <div className="project-summary-links">
+          <a href={github} className="project-summary-link">
+            <FontAwesomeIcon
+              className="project-summary-link-icon"
+              icon={faGithub}
+            />
+            <div className="project-summary-link-label">Source</div>
+          </a>
 
-  //     <Image fluid={image.fluid} style={{ height: "200px", width: "200px" }} />
-  //     <br></br>
-  //     <Link to="/projects/">Back to projects</Link>
-  //   </Layout>
-  // )
+          {!!website && (
+            <a href={website} className="project-summary-link">
+              <FontAwesomeIcon
+                className="project-summary-link-icon"
+                icon={faExternalLinkAlt}
+              />
+              <div className="project-summary-link-label">View Project</div>
+            </a>
+          )}
+        </div>
+
+        <div className="project-summary">
+          <div className="project-summary-description">{summary}</div>
+
+          <ul className="project-summary-technologies">
+            {technologies.map(technology => (
+              <li className="project-summary-technology">{technology.name}</li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="project-summary-body">
+          <MDXRenderer>{mdx}</MDXRenderer>
+        </div>
+      </div>
+    </Layout>
+  )
 }
